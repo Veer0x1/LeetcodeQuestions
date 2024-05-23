9cf82879b5341b2d9de5a24cf7e3377b06a763eb
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2024-05-24 02:41:26 +0530

    tanstack tables done

---
+++ app/questions/data/schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const questionSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  status: z.string(),
+  label: z.string(),
+  priority: z.string(),
+});
+
+export type Task = z.infer<typeof questionSchema>;
