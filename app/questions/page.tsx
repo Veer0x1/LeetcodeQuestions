0352600495727c017b8eb53d80bc4bf9f0646032
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2024-05-24 01:00:32 +0530

    Initial setup with basic components and utilities

---
+++ app/questions/page.tsx
@@ -0,0 +1,13 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { Metadata } from "next";
+import { z } from "zod";
+
+
+export default function Questions(){
+    return(
+        <div>
+            <h1>Questions</h1>
+        </div>
+    )
+}
